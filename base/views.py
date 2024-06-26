17b06c24a9ba79e9468fabddac57b6dfa35aaed5
Manoram <manoramsubedi7@gmail.com>
2024-06-26 20:24:00 +0545

    registration model added

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
@@ -18,13 +19,14 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('home')
 
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
 
@@ -43,12 +45,30 @@ def loginPage(request):
 
 
 
-    context = {}
+    context = {'page':page}
     return render(request, "base/login_register.html", context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    #page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.add_message(request, messages.INFO, "An error occured during registration")
+
+    context = {'form':form}
+    return render(request, 'base/login_register.html', context)
 
 
 
